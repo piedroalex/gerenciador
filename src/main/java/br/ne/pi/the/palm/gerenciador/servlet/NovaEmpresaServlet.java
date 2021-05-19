d19acc0184054d39a567c0eb7726fc38a157cf3d
Pedro Alex L Martins <piedroalex@gmail.com>
2021-05-19 18:29:07 -0300

    Trabalhando com o método doPost.

---
+++ src/main/java/br/ne/pi/the/palm/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,27 @@
+package br.ne.pi.the.palm.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaempresa")	
+public class NovaEmpresaServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa!");
+		String nomeEmpresa = request.getParameter("nome");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
+	}
+
+}
